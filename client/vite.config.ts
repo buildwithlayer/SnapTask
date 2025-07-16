5ae0db48f640e6eb4c351a3c892eebbd61722bf3
opticayaan <ayaan.gill@me.com>
2025-07-16 15:57:23 -0400

    Move allowed hosts to env var

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,18 @@
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd());
+    return {
     build: {
         outDir: '../dist',
     },
     plugins: [react(), tailwindcss(), svgr()],
     server: {
-        allowedHosts: ['b4be9442782e.ngrok-free.app'],
+        allowedHosts: env.VITE_ALLOWED_HOSTS?.split(',') || [],
         proxy: {
             '/api': {
                 changeOrigin: true,
@@ -18,4 +20,4 @@ export default defineConfig({
             },
         },
     },
-});
+}});
